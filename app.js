8a26ceb2595961d8d2d508b83e3358e00a184933
Shubh Shahu <shubhshahu.01@gmail.com>
2023-04-05 15:31:25 +0530

    tempConverter

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const buttons = document.querySelectorAll("input[type=button]");
 const display = document.querySelector("input[type=text]");
+const radios = document.querySelectorAll("input[type=radio]");
+const convert = document.querySelector("#convert");
 
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
     let n = 0;
-    if (button.value == "=") {
-      display.value = eval(display.value);
+    if (button.value == "Convert") {
+      display.value = converter(parseInt(display.value));
     } else if (button.value == "AC") {
       display.value = "";
     } else if (button.value == "DEL") {
@@ -16,3 +18,24 @@ Array.from(buttons).forEach((button) => {
     }
   });
 });
+
+function converter(a) {
+  let xyz;
+  for (let radio of radios) {
+    if (radio.checked) {
+      xyz = radio.value;
+      break;
+    }
+  }
+  if (xyz == "cel") {
+    return `${(a = a * (9 / 5) + 32)} \u00B0F`;
+  } else if (xyz == "far") {
+    return `${(a = (a - 32) * (5 / 9))} \u00B0C`;
+  } else if (xyz == "kel") {
+    return `${(a = a + 273)} K`;
+  } else if (xyz == "kc") {
+    return `${(a = a - 273)} \u00B0C`;
+  } else {
+    return "select a conversion";
+  }
+}
